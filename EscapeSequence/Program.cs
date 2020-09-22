964e2964070d47746052531f720656344e1580d3
LaurieSue <pink.bee.me@outlook.com>
2020-09-22 00:11:17 -0500

    Add project files.

---
+++ EscapeSequence/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EscapeSequence
+{
+    class Program
+    {
+        static void Main()
+        {
+            string doubleQuotes = "Hold up! Wait a minute!... Nevermind. Just \" kidding \" !!!";
+            Console.WriteLine(doubleQuotes);
+            Console.ReadLine();
+
+        }
+    }
+}
